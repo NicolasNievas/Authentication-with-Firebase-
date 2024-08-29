b2a460a2e51382fc73409b506d05a403cc7d8a62
Nicolas Nievas <nicoonievas7@gmail.com>
2024-08-28 23:41:23 -0300

    Create SignInWithGoogle.ts

---
+++ firebase-app/src/services/auth/SignInWithGoogle.ts
@@ -0,0 +1,21 @@
+import app from "../firebase";
+import { getAuth, signInWithPopup, User, GoogleAuthProvider } from "firebase/auth";
+import ErrorHandler from "@/helper/errorHandler.helper";
+import { AuthResponse, RESPONSE_STATUS } from "@/interfaces/data";
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const signInWithGoogle = async (): Promise<AuthResponse<User>> => {
+    try{
+        const { user } = await signInWithPopup(auth, provider);
+        return { response: user, status: RESPONSE_STATUS.CREATED, message: "Sign in successfully." };
+    } catch (error: any){
+        const errorMessage = error.message;
+        const errorCode = error.code;
+        const message = ErrorHandler(errorCode, errorMessage);
+        return { message, status: RESPONSE_STATUS.ERROR };
+    }
+}
+
+export default signInWithGoogle;

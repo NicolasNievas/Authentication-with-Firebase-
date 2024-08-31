8d24525b6bf95b6b4d0e9c90987e0130f246321c
Nicolas Nievas <nicoonievas7@gmail.com>
2024-08-31 14:03:31 -0300

    styles footer

--- firebase-app/src/components/Footer.tsx
+++ firebase-app/src/components/Footer.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: 'black', color: 'white', padding: '10px 0', textAlign: 'center' }}>
+    <footer className="bg-black text-white py-4 text-center">
       <div className="container mx-auto">
         <p className="text-sm">
-        <a href="https://www.linkedin.com/in/nievas-nicolas/" target="_blank" rel="noopener noreferrer" className=""> © 2024 Nicolas Nievas</a>        </p>
-        <p className="text-sm">
-          <a href="https://github.com/NicolasNievas/Authentication-with-Firebase-" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-600">GitHub Repository</a>
+          <a href="https://www.linkedin.com/in/nievas-nicolas/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
+            © 2024 Nicolas Nievas
+          </a>
+        </p>
+        <p className="text-sm mt-2">
+          <a href="https://github.com/NicolasNievas/Authentication-with-Firebase-" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">
+            GitHub Repository
+          </a>
         </p>
       </div>
     </footer>

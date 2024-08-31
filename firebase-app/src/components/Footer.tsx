22fb01ea7a3581b2e356254afcb1d4be2b08f02c
Nicolas Nievas <nicoonievas7@gmail.com>
2024-08-31 14:02:50 -0300

    url github

--- firebase-app/src/components/Footer.tsx
+++ firebase-app/src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <p className="text-sm">
         <a href="https://www.linkedin.com/in/nievas-nicolas/" target="_blank" rel="noopener noreferrer" className=""> © 2024 Nicolas Nievas</a>        </p>
         <p className="text-sm">
-          <a href="https://github.com/NicolasNievas" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-600">GitHub Repository</a>
+          <a href="https://github.com/NicolasNievas/Authentication-with-Firebase-" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-600">GitHub Repository</a>
         </p>
       </div>
     </footer>
